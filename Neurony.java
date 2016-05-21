89fa3bbe8461c775056f95c457fad233df2f12e5
Tomasz Lisek <t.lisek.agh@gmail.com>
2016-05-21 12:34:23 +0200

    Naprawiony błąd

--- Neurony.java
+++ Neurony.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 public class Neurony {
   public static void main(String args[]) {
 
-    double[] zapalarka = new double[]() {1, 1.5, 3, 2, 9, 2.1};
+    // double[] zapalarka = new double[6]() {1, 1.5, 3, 2, 9, 2.1};
 
     WczytajZPliku test = new WczytajZPliku();
     SiecNeuronowa nowaMapa = new SiecNeuronowa(test.tablica);
