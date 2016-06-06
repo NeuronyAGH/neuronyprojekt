d44f0ff27483745b7c2165b922101c18974dbbc2
Tomasz Lisek <t.lisek.agh@gmail.com>
2016-06-06 17:46:50 +0200

    Poprawka (moc sygnału)

--- Neurony.java
+++ Neurony.java
@@ -14,38 +14,32 @@ public class Neurony {
 
     double[] zapalarka = {1, 1.5, 3, 2, 9, 2.1};
 
-    WczytajZPliku nowyPlik = new WczytajZPliku();
-    SiecNeuronowa nowaSiec = new SiecNeuronowa(nowyPlik.tablica);
+    WczytajZPliku test = new WczytajZPliku();
+    SiecNeuronowa nowaSiec = new SiecNeuronowa(test.tablica);
     nowaSiec.rozpal(zapalarka);
   }
 }
 
-class ObjectNeuron {
+class ValueNeuron {
   double wartosc;
   String kolumna;
   double moc;
-  ObjectNeuron(double wartosc, String kolumna){
+  ValueNeuron(double wartosc, String kolumna){
     this.wartosc = wartosc;
     this.kolumna = kolumna;
   }
-  void printObjectNeuron(){
+  void printValueNeuron(){
     System.out.print("["+ wartosc + " : " + kolumna +"]");
   }
 }
-class ValueNeuron {
-  String nazwa;
-  double moc;
-  ValueNeuron(String nazwa){
-    this.nazwa = nazwa;
-  }
-}
-
 class SiecNeuronowa {
-  List<ObjectNeuron> wypisaneValueNeurons = new ArrayList<ObjectNeuron>();
+  Map<ValueNeuron, Double> moceValueNeurons = new HashMap<ValueNeuron, Double>();
+  Map<String, Double> moceObjectNeurons = new HashMap<String, Double>();
+  List<ValueNeuron> wypisaneValueNeurons = new ArrayList<ValueNeuron>();
   List<String> wypisaneObjectNeurons = new ArrayList<String>();
   Map<String, Map<Double, List<String>>> kolumny = new HashMap<String, Map<Double, List<String>>>();
-  Map<String, List<ObjectNeuron>> neurony = new HashMap<String, List<ObjectNeuron>>();
-  Map<Double, List<ObjectNeuron>> odleglosci = new HashMap<Double, List<ObjectNeuron>>();
+  Map<String, List<ValueNeuron>> neurony = new HashMap<String, List<ValueNeuron>>();
+  Map<Double, List<ValueNeuron>> odleglosci = new HashMap<Double, List<ValueNeuron>>();
 
   SiecNeuronowa(double[][] t){
 
@@ -73,35 +67,32 @@ class SiecNeuronowa {
     // TWORZENIE NEURONÓW DLA WARTOŚCI W WEDŁUG KOLUMN
 
     for(int i = 0; i<tablicaWejsciowa[0].length; i++ ){
-      Map<Double, List<String>> neuronywKolumnie = new HashMap<Double, List<String>>();
+      Map<Double, List<String>> neuronywKolumnie = new HashMap<Double, List<String>>(); //Tworzenie pustej mapy połączeń dla każdej kolumny
 
       if(kolumny.get("C"+i)==null){
-        kolumny.put("C"+i, neuronywKolumnie);
+        kolumny.put("C"+i, neuronywKolumnie); //Włożenie jej w odpowiednią kolumnę
       }
       for(int j=0;j<tablicaWejsciowa.length;j++){
-
-
         double wartosc = tablicaWejsciowa[j][i];
         if(kolumny.get("C"+i).get(wartosc)==null){
-          kolumny.get("C"+i).put(wartosc,new ArrayList<String>());
+          kolumny.get("C"+i).put(wartosc,new ArrayList<String>()); //Dodanie wartości (wraz z pustą listą połączeń) w do kolumny
         }
       }
     }
 
-
     // TWORZENIE NEURONÓW DLA KAŻDEGO WIERSZA (Z PUSTĄ LISTĄ POŁĄCZEŃ)
 
     for(int i = 0; i<tablicaWejsciowa.length; i++ ){
-      List<ObjectNeuron> wartosci = new ArrayList<ObjectNeuron>();
-      neurony.put("N"+i, wartosci);
+      List<ValueNeuron> wartosci = new ArrayList<ValueNeuron>(); //Tworzenie pustej listy połączeń z wartościami
+      neurony.put("O"+i, wartosci); //I dodanie jej do każdego ObjectNeuronu
 
       // ŁACZENIE NEURONÓW Z WARTOŚCIAMI (WYPEŁNIANIE LISTY POŁĄCZEŃ)
 
       for(int j=0;j<tablicaWejsciowa[i].length;j++){
         double wartosc = tablicaWejsciowa[i][j];
-        ObjectNeuron noweObjectNeuron = new ObjectNeuron(wartosc, "C"+j);
-        neurony.get("N"+i).add(noweObjectNeuron);
-        kolumny.get("C"+j).get(wartosc).add("N"+i);
+        ValueNeuron nowyValueNeuron = new ValueNeuron(wartosc, "C"+j);
+        neurony.get("O"+i).add(nowyValueNeuron); //Dodanie połączenia ObjectNeuron -> Wartość w kolumnie
+        kolumny.get("C"+j).get(wartosc).add("O"+i); //Dodanie połączenia Wartość w kolumnie -> ObjectNeuron
       }
     }
 
@@ -129,17 +120,17 @@ class SiecNeuronowa {
     for(String key : k_neurony){
       System.out.printf("\t" + key + " -> ");
 
-      List<ObjectNeuron> polaczenia =  neurony.get(key);
-      for(ObjectNeuron ObjectNeuron : polaczenia){
-        ObjectNeuron.printObjectNeuron();
+      List<ValueNeuron> polaczenia =  neurony.get(key);
+      for(ValueNeuron ValueNeuron : polaczenia){
+        ValueNeuron.printValueNeuron();
         System.out.printf(" ");
       }
       System.out.printf("\n");
     }
   }
 
-  boolean containsPolaczenie(List<ObjectNeuron> list, ObjectNeuron el){
-    for (ObjectNeuron e : list) {
+  boolean containsPolaczenie(List<ValueNeuron> list, ValueNeuron el){
+    for (ValueNeuron e : list) {
       if (e.wartosc == el.wartosc && e.kolumna.equals(el.kolumna)) {
         return true;
       }
@@ -147,30 +138,50 @@ class SiecNeuronowa {
     return false;
   }
 
-  void wypiszNeuron(ObjectNeuron valueNeuron){
-    wypisaneValueNeurons.add(valueNeuron);
-    valueNeuron.printObjectNeuron();
-    System.out.print("\n");
-    List<String> polaczenia = kolumny.get(valueNeuron.kolumna).get(valueNeuron.wartosc);
+  void wypiszNeuron(ValueNeuron valueNeuron, double power){ //Wypisywanie valueNeuronu
+    if(moceValueNeurons.get(valueNeuron) == null){ //Sprawdz, czy już istnieje w tablicy z mocami
+      moceValueNeurons.put(valueNeuron, power); //Jeśli nie to dodaj z mocą
+    }
+    else { //Jeśli tak to zwiększ moc neuronu
+      double newPower = moceValueNeurons.get(valueNeuron) + power;
+      moceValueNeurons.put(valueNeuron, newPower);
+    }
+    if(moceValueNeurons.get(valueNeuron)>0.5){ //Jeżeli moc > 0.5 wypisz neuron i dodaj do listy wypisanych, aby się nie powtarzał
+      wypisaneValueNeurons.add(valueNeuron);
+      valueNeuron.printValueNeuron();
+      System.out.print(" (moc: " + moceValueNeurons.get(valueNeuron) + ")\n");
+      // System.out.println(moceValueNeurons.get(valueNeuron));
+    }
+    List<String> polaczenia = kolumny.get(valueNeuron.kolumna).get(valueNeuron.wartosc); //Wypisz każde połączenie
     if(polaczenia.size()>0){
       for(String polaczenie : polaczenia){
-        if(!wypisaneObjectNeurons.contains(polaczenie)){
-          wypiszNeuron(polaczenie);
+        if(!wypisaneObjectNeurons.contains(polaczenie) && power > 0.001){ //Jeśli jeszcze jest moc
+          wypiszNeuron(polaczenie, power/2);
         }
       }
     }
   }
-  void wypiszNeuron(String objectNeuron){
-    wypisaneObjectNeurons.add(objectNeuron);
-    System.out.println(objectNeuron);
-    List<ObjectNeuron> polaczenia = neurony.get(objectNeuron);
+  void wypiszNeuron(String objectNeuron, double power){ //Analogicznie jak wyżej, ale dla objectNeuron
+    if(moceObjectNeurons.get(objectNeuron) == null){
+      moceObjectNeurons.put(objectNeuron, power);
+    }
+    else {
+      double newPower = moceObjectNeurons.get(objectNeuron) + power;
+      moceObjectNeurons.put(objectNeuron, newPower);
+    }
+    if(moceObjectNeurons.get(objectNeuron)>0.5){
+      System.out.print(objectNeuron + " (moc: " + moceObjectNeurons.get(objectNeuron) + ")\n");
+      // System.out.println(moceObjectNeurons.get(objectNeuron));
+      wypisaneObjectNeurons.add(objectNeuron);
+    }
+
+    List<ValueNeuron> polaczenia = neurony.get(objectNeuron);
     if(polaczenia.size()>0){
-      for(ObjectNeuron polaczenie : polaczenia){
-
-        // System.out.println(containsPolaczenie(siec.wypisaneValueNeurons, polaczenie));
-
-        if(!containsPolaczenie(wypisaneValueNeurons, polaczenie)){
-          wypiszNeuron(polaczenie);
+      for(ValueNeuron polaczenie : polaczenia){
+        if(!containsPolaczenie(wypisaneValueNeurons, polaczenie) && power > 0.001){
+        // if(!wypisaneValueNeurons.contains(polaczenie) && probujDalej){
+          // System.out.print("\t");
+          wypiszNeuron(polaczenie, power/2);
         }
       }
     }
@@ -182,36 +193,36 @@ class SiecNeuronowa {
 
     for(int i=0; i<zapalarka.length;i++){
       String key = "C"+i;
-      Map<Double, List<String>> kolumna = kolumny.get(key);
+      Map<Double, List<String>> kolumna = kolumny.get(key); //Tablica sortująca valueNeurony wedługo odległości od bodźca
       List<Double> values = new ArrayList<Double>(kolumna.keySet());
       for(double value : values){
-        double odleglosc = Math.round(Math.abs(value-zapalarka[i])*100);
+        double odleglosc = Math.round(Math.abs(value-zapalarka[i])*100); //Liczenie odległości
         odleglosc /= 100;
         if(odleglosci.get(odleglosc)==null){
-          odleglosci.put(odleglosc, new ArrayList<ObjectNeuron>());
+          odleglosci.put(odleglosc, new ArrayList<ValueNeuron>()); //Dodawanie połączenia wg. odległości
         }
-        ObjectNeuron noweObjectNeuron = new ObjectNeuron(value, "C"+i);
-        odleglosci.get(odleglosc).add(noweObjectNeuron);
+        ValueNeuron noweValueNeuron = new ValueNeuron(value, "C"+i);
+        odleglosci.get(odleglosc).add(noweValueNeuron);
       }
     }
 
-    System.out.println("\nTABLICA ODLEGŁOŚCI:\n");
+    System.out.println("\nTABLICA ODLEGŁOŚCI:\n"); //Wypisiwanie tablicy odległości
 
     List<Double> sortowaneOdleglosci = new ArrayList<Double>(odleglosci.keySet());
-    Collections.sort(sortowaneOdleglosci);
+    Collections.sort(sortowaneOdleglosci); //Sortowanie listy wedługo odległości
     for(double key : sortowaneOdleglosci){
       System.out.print(key + ": ");
-      for(ObjectNeuron el : odleglosci.get(key)){
-        el.printObjectNeuron();
+      for(ValueNeuron el : odleglosci.get(key)){
+        el.printValueNeuron();
       }
       System.out.print("\n");
     }
 
     System.out.println("\nWYPISYWANIE:\n");
     for(double key : sortowaneOdleglosci){
-      for(ObjectNeuron el : odleglosci.get(key)){
+      for(ValueNeuron el : odleglosci.get(key)){
         if(!containsPolaczenie(wypisaneValueNeurons, el)){
-          wypiszNeuron(el);
+          wypiszNeuron(el,1); //wypisywanie każdego valueNeuronu (najbliższe najpierw)
         }
       }
     }
